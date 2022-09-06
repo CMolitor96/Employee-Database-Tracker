939338cc8da9823d2b525fe8c8d3436229a21aa6
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2022-09-06 14:12:46 -0700

    Added list of initial choices to inquirer prompt and added if else statement to handle initial table viewing of department, role, and employee

--- server.js
+++ server.js
@@ -12,6 +12,71 @@ const db = mysql.createConnection(
     },
 );
 
+const questions = [
+    {
+        type: 'input',
+        message: 'View All Departments',
+        name: 'viewDepart'
+    },
+    {
+        type: 'input',
+        message: 'View All Roles',
+        name: 'viewRoles'
+    },
+    {
+        type: 'input',
+        message: 'View All Employees',
+        name: 'viewEmployee'
+    },
+    {
+        type: 'input',
+        message: 'Add a Department',
+        name: 'addDepartment'
+    },
+    {
+        type: 'input',
+        message: 'Add a Role',
+        name: 'addRole'
+    },
+    {
+        type: 'input',
+        message: 'Add an Employee',
+        name: 'addEmployee'
+    },
+    {
+        type: 'input',
+        message: 'Update Employee Role',
+        name: 'updateEmployee'
+    }
+];
+
+const questions1 = [
+    {
+        type: 'list',
+        name: 'choice',
+        message: 'Please select from the following options: ',
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee Role']
+    }
+];
+
+inquirer
+    .prompt(questions1).then(response => {
+        if (response.choice === 'View All Departments') {
+            console.log('success');
+            db.query(`SELECT * FROM department`, (err, results) => {
+                console.table(results);
+
+            })
+        } else if (response.choice === 'View All Roles') {
+            db.query(`SELECT * FROM roles`, (err, results) => {
+                console.table(results);
+            })
+        } else if (response.choice === 'View All Roles') {
+            db.query(`SELECT * FROM roles`, (err, results) => {
+                console.table(results);
+            })
+        }
+    });
 
 
 // const promise = new Promise((resolve, reject) => {
@@ -47,19 +112,19 @@ const db = mysql.createConnection(
 //     console.table(results);
 // });
 // test();
-inquirer
-    .prompt([{
-        type: 'input',
-        name: 'name',
-        message: 'What is your name? '
-    }])
-    .then(response => {
-        console.log(response.name);
-        const mysqlPromise = require('mysql2/promise');
-        let goose = mysqlPromise.createConnection({ host: process.env.host, user: process.env.user, password: process.env.password, database: process.env.database })
-        .then(conn => conn.query(`SELECT * from employee`))
-        .then(table => console.log(table[0]));
-    });
+// inquirer
+//     .prompt([{
+//         type: 'input',
+//         name: 'name',
+//         message: 'What is your name? '
+//     }])
+//     .then(response => {
+//         console.log(response.name);
+//         const mysqlPromise = require('mysql2/promise');
+//         let goose = mysqlPromise.createConnection({ host: process.env.host, user: process.env.user, password: process.env.password, database: process.env.database })
+//         .then(conn => conn.query(`SELECT * from department`))
+//         .then(table => console.table(table[0]));
+//     });
 
 
 
