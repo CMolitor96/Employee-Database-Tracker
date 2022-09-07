657da3fc620698b15f26caf3fba88d7e9b282426
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2022-09-06 17:51:26 -0700

    Added 2 more functions, one for adding a department, and one for adding a role, which as of now does not actually update database, but is ready for code. Slightly figured out async functions and how to callback original function, now need to write functions for following questions

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,18 @@ CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
+ALTER TABLE role AUTO_INCREMENT=1000;
 
 CREATE TABLE employee (
     id INT PRIMARY KEY,
--- server.js
+++ server.js
@@ -50,34 +50,106 @@ const questions = [
     }
 ];
 
-const questions1 = [
+const initialQuestions = [
     {
         type: 'list',
         name: 'choice',
         message: 'Please select from the following options: ',
-        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee Role']
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee Role', 'All finished for now']
     }
 ];
 
-inquirer
-    .prompt(questions1).then(response => {
-        if (response.choice === 'View All Departments') {
-            console.log('success');
-            db.query(`SELECT * FROM department`, (err, results) => {
-                console.table(results);
+// inquirer
+//     .prompt(initialQuestions).then(response => {
+//         if (response.choice === 'View All Departments') {
+//             console.log('success');
+//             db.query(`SELECT * FROM department`, (err, results) => {
+//                 console.table(results);
 
-            })
-        } else if (response.choice === 'View All Roles') {
-            db.query(`SELECT * FROM roles`, (err, results) => {
-                console.table(results);
-            })
-        } else if (response.choice === 'View All Roles') {
-            db.query(`SELECT * FROM roles`, (err, results) => {
-                console.table(results);
-            })
+//             })
+//         } else if (response.choice === 'View All Roles') {
+//             db.query(`SELECT * FROM role`, (err, results) => {
+//                 console.table(results);
+//             })
+//         } else if (response.choice === 'View All Employees') {
+//             db.query(`SELECT * FROM employee`, (err, results) => {
+//                 console.table(results);
+//             })
+//         } else if (response.choice === 'All finished for now') {
+//             console.log('Have a nice day :)');
+//             return;
+//         }
+//     });
+
+function initialQuestion() {
+    inquirer.prompt(initialQuestions).then(response => { viewTables(response) })
+}
+initialQuestion();
+
+function viewTables(response) {
+    if (response.choice === 'View All Departments') {
+        db.query(`SELECT * FROM department`, (err, results) => {
+            console.table(results);
+            initialQuestion();
+
+        })
+    } else if (response.choice === 'View All Roles') {
+        db.query(`SELECT * FROM role`, (err, results) => {
+            console.table(results);
+            initialQuestion();
+        })
+    } else if (response.choice === 'View All Employees') {
+        db.query(`SELECT * FROM employee`, (err, results) => {
+            console.table(results);
+            initialQuestion();
+        })
+    } else if (response.choice === 'All finished for now') {
+        console.log('Have a nice day :)');
+        return;
+    } else if (response.choice === 'Add a Department') {
+        addDepartment();
+    } else if (response.choice === 'Add a Role') {
+        addRole();
+    }
+};
+
+async function addDepartment() {
+    await inquirer.prompt([{ type: 'input', message: 'Department Name: ', name: 'departmentName' }]).then(response => {
+        console.log('Department Sucessfully Added');
+        db.query(`INSERT INTO department (department_name) VALUES (?)`, response.departmentName, (err, result) => { });
+    });
+    initialQuestion();
+}
+
+async function addRole() {
+    let departmentArray = [];
+    db.query(`SELECT department_name FROM department`, (err, results) => {
+        for (i = 0; i < results.length; i++) {
+            departmentArray.push(results[i].department_name)
         }
     });
-
+    await inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Role Name: ',
+            name: 'roleName'
+        },
+        {
+            type: 'input',
+            message: 'Salary for Role: ',
+            name: 'salaryRole'
+        },
+        {
+            type: 'list',
+            message: 'Deparatment in which role belongs: ',
+            name: 'roleDepartment',
+            choices: departmentArray
+        }
+    ]).then(response => {
+        console.log(response.roleName, response.salaryRole, response.roleDepartment);
+    });
+    initialQuestion();
+}
 
 // const promise = new Promise((resolve, reject) => {
 //     resolve(db.query(`SELECT * from department`, (err, results) => {
